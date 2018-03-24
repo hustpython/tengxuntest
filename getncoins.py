d62df78ea328a5550b1bbda26e1945b821900757
mxq <1529806383@qq.com>
2018-03-24 15:43:17 +0800

    s

--- getncoins.py
+++ getncoins.py
@@ -3,6 +3,12 @@
 # 一个土豪拥有的硬币面额及数量如下（注意每个面额只有两个）
 # 1 1 2 2 4 4 8 8... 
 # 求给定一个正整数n，能够获得的总额为n的硬币组合数
+# solution:
+# 将N转化为二进制，通过移动末尾的0,移动得到的新的二进制距离N的差距通过
+# 另外一个二进制得到补偿，然后将这两个互补的二进制转化为十进制即可。
+# 14 = 1110->1101->1011->0111
+#      0000->0001->0011->0111
+
 from collections import defaultdict
 
 def getpermutation(n):
@@ -27,11 +33,13 @@ def getpermutation(n):
                     binary_n[i] = '0'
                     binary_n[i+1] = new_bin[i+1] = '1'
                     flag = True 
+                    print(binary_n,new_bin)
                     res += 1
                 elif new_bin[i] == '1':
                     new_bin[i] = '0'
                     binary_n[i+1] = new_bin[i+1] = '1'
                     flag = True 
+                    print(binary_n,new_bin)
                     res += 1
 
     return res + 1
